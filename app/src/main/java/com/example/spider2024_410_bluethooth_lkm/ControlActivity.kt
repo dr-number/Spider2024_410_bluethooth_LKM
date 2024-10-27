4d8c8364ff9275fce4a2cdce32fed7557fe22afa
Lar97 <dr.number@yandex.ru>
2024-10-27 07:54:13 +0700

    correct control

--- app/src/main/java/com/example/spider2024_410_bluethooth_lkm/ControlActivity.kt
+++ app/src/main/java/com/example/spider2024_410_bluethooth_lkm/ControlActivity.kt
@@ -9,6 +9,10 @@ import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.control_layout.*
@@ -34,19 +38,84 @@ class ControlActivity: AppCompatActivity(){
 
         ConnectToDevice(this).execute()
 
-        btn_up.setOnClickListener{ sendCommand("a") } // apertando "a" no app, ativa o led no arduino
-        btn_down.setOnClickListener{ sendCommand("b") } // apertando "b" no app, ativa o led no arduino
-        btn_left.setOnClickListener{sendCommand("c")}
-        btn_right.setOnClickListener { sendCommand("d") }
+        setButtonTouchListener( btn_forward, 'W') // apertando "a" no app, ativa o led no arduino
+        setButtonTouchListener( btn_back, 'S') // apertando "a" no app, ativa o led no arduino
+        setButtonTouchListener( btn_left, 'D') // apertando "a" no app, ativa o led no arduino
+        setButtonTouchListener( btn_right, 'A') // apertando "a" no app, ativa o led no arduino
+
+
+        btn_stand.setOnClickListener {
+            sendCommand("F") // Начать выполнение команды при нажатии кнопки
+        }
+        btn_vverh.setOnClickListener {
+            sendCommand("R") // Начать выполнение команды при нажатии кнопки
+        }
+
+
         btn_disconnect.setOnClickListener{ disconnect() }
         btn_connect.setOnClickListener{ ConnectToDevice(this).execute() }
 
+    }
+    private fun setButtonTouchListener(button: View, command: Char) {
+        button.setOnClickListener { event ->
+                sendCommand(command)
+        }
+//        button.setOnTouchListener { _, event ->
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> {
+//                    sendCommand(command)
+//                    true
+//                }
+//                MotionEvent.ACTION_UP -> {
+//                    //sendCommand('F') // Остановка при отпускании кнопки
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+    }
+
+//    private fun sendCommand(command: Char) {
+//        try {
+//            outputStream?.write(command.toInt())
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//            Toast.makeText(this, "Ошибка отправки команды", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+    private fun sendCommand(input: Char){
+        if(m_bluetoothSocket != null){
+            try{
+                var ddd = input.toInt()
+                //for(i in 1..5000) {
+                    m_bluetoothSocket!!.outputStream.write(ddd)
+                //}
+            }catch(e: IOException){
+                Toast.makeText(this, "Error send command!", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun sendCommand(input: String){
         if(m_bluetoothSocket != null){
             try{
                 m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+            }catch(e: IOException){
+                Toast.makeText(this, "Error send command!", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun sendCommandMany(input: String){
+        if(m_bluetoothSocket != null){
+            try{
+                for(i in 1..2000) {
+                    m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+                }
             }catch(e: IOException){
                 Toast.makeText(this, "Error send command!", Toast.LENGTH_SHORT).show()
                 e.printStackTrace()
